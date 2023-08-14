eadef6b28a4ad21b10726a55f2255262ffb98e9c
1 <a1@1s-MacBook-Pro.local>
2023-08-14 10:54:23 +0530

    added functionality of multer upload

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 require('./models/db')
 const express=require ('express')
 const mongoose=require('mongoose')
-const uploads=mongoose.model('uploadDetails')
+const upload_img=mongoose.model('uploadDetails')
 const app=express();
 const multer=require('multer')
-// const bodyParser= require('body-parser')
-// app.use(bodyParser.urlencoded({ extended: false }))
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine','ejs');
 app.use(express.static('public'));
-var image_name='';
+var adhar_img='';
+var pan_img='';
+var voter_img='';
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-      cb(null, 'uploads/');
+      cb(null, 'public/uploads');
     },
     filename: function(req, file, cb) {
-        img_name=file.originalname;
+      adhar_img=file.originalname;
+      pan_img=file.originalname;
+      voter_img=file.originalname;
       cb(null, file.originalname);
     }
   });
@@ -22,9 +26,46 @@ const storage = multer.diskStorage({
 app.get('/',(req,res)=>{
     res.render('index')
 })
-app.post('/aadhar',upload.single("adhar"),(req,res)=>{
-    var x=req.body;
+app.post('/aadhar',upload.single('adhar_img'),(req,res)=>{
+  console.log(req.body.adhar_img)
+    var x= new upload_img(req.body);
     console.log(x)
+    x.adhar_img=adhar_img
+    x.save()
+    .then((result)=>{
+      console.log('data saved')
+    })
+    .catch((err)=>{
+      console.log('error'+err)
+    })
+    res.redirect('/')
+})
+app.post('/pan',upload.single('pan_img'),(req,res)=>{
+  console.log(req.body.pan_img)
+    var x= new upload_img(req.body);
+    console.log(x)
+    x.pan_img=pan_img
+    x.save()
+    .then((result)=>{
+      console.log('data saved')
+    })
+    .catch((err)=>{
+      console.log('error'+err)
+    })
+    res.redirect('/')
+})
+app.post('/voter',upload.single('voter_img'),(req,res)=>{
+  console.log(req.body.voter_img)
+    var x= new upload_img(req.body);
+    console.log(x)
+    x.voter_img=voter_img
+    x.save()
+    .then((result)=>{
+      console.log('data saved')
+    })
+    .catch((err)=>{
+      console.log('error'+err)
+    })
     res.redirect('/')
 })
 app.listen(3000,()=>{
--- models/upload.model.js
+++ models/upload.model.js
@@ -1,5 +1,7 @@
 const mongoose=require ('mongoose');
 const uploadSchema= new mongoose.Schema({
-    img_name:String
+    adhar_img:{type:'String',default:'NA'},
+    pan_img:{type:'String',default:'NA'},
+    voter_img:{type:'String',default:'NA'}
 })
 mongoose.model('uploadDetails',uploadSchema)
